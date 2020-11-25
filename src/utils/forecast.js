968b3b2449b0fc574a6b5e2673f752cc9463c7ca
Andrei Alexandru Caia <andreialexx30@gmail.com>
2020-11-26 00:26:26 +0200

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&units=metric&exclude=&appid=aa77d9cde24d1d4544789390ef354868';
+
+    request( { url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather network!', undefined);
+        }else if(body.error){
+            callback('Unable to find location', undefined);
+        }else {
+            callback(undefined, `${body.current.weather[0].description}, it is currently ${body.current.temp} degrees out. But it feels more like ${body.current.feels_like} degrees`);
+        }
+    })
+}
+
+module.exports = forecast;
+
+
+//https://api.openweathermap.org/data/2.5/onecall?lat=46.770439&lon=23.591423&units=metric&exclude=hourly,daily&appid=aa77d9cde24d1d4544789390ef354868
